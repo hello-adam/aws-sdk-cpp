22bda8127285fed069b6281e051f4c13e8a0ef17
Adam Nash <adam@smr.llc>
2023-11-25 23:01:22 -0500

    try to fix windows install dir by not using a variable

--- build.bat
+++ build.bat
@@ -1,7 +1,4 @@
 
-SET /A INSTALL_DIR=%~dp0%dist
-echo Base Dir: %~dp0
-echo Install Dir: %INSTALL_DIR%
 
 cmake -B builddeps -S thirdparty -G "Visual Studio 17 2022" -DCMAKE_BUILD_TYPE=MinSizeRel -A x64 -T v141 || goto :error
 cmake --build builddeps || goto :error
@@ -15,11 +12,11 @@ cmake -G Ninja -B build ^
  -DBUILD_ONLY="cognito-idp;cognito-identity;dynamodb" ^
  -DBUILD_SHARED_LIBS=OFF ^
  -DCMAKE_POSITION_INDEPENDENT_CODE=ON ^
- -DCMAKE_INSTALL_PREFIX=%INSTALL_DIR% ^
+ -DCMAKE_INSTALL_PREFIX=%~dp0%dist ^
  -DAWS_SDK_WARNINGS_ARE_ERRORS=OFF ^
- -DOPENSSL_ROOT_DIR=%INSTALL_DIR% ^
+ -DOPENSSL_ROOT_DIR=%~dp0%dist ^
  -DOPENSSL_USE_STATIC_LIBS=ON ^
- -DZLIB_ROOT=%INSTALL_DIR% ^
+ -DZLIB_ROOT=%~dp0%dist ^
  -DZLIB_USE_STATIC_LIBS=ON || goto :error
  
 cmake --build build || goto :error
